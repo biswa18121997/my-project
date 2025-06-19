c846c53edb4b14e982d841b796f7a7e704525af2
MaheshPokale99 <pokalemahesh98@gmail.com>
2025-06-19 20:41:43 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import CustomeText from "@/components/ui/CustomeText";
 import DualToggleButtons from "@/components/ui/DualButtons";
 import Image from "next/image";
 import { Star } from "lucide-react";
+import OrangeButton from "@/components/ui/OrangeButton";
 
 export default function Home() {
   const experiences = [
@@ -29,13 +30,17 @@ export default function Home() {
     },
   ];
 
+  const buttons = [
+    "Landing Page", "Product Deesign", "Animation", "Glassmorphim", "Cards"
+  ]
+
   return (
     <div className="relative min-h-screen w-full bg-white py-10 flex flex-col items-center justify-start">
       <Navbar />
 
       {/* Hero Section */}
       <div className="group flex max-w-[1440px] w-full h-[846px] px-4 gap-[71px] items-center justify-center mt-6">
-        
+
         {/* Left Testimonial */}
         <div className="flex flex-col w-[328px] h-[138px] items-start justify-start transition-transform duration-300 ease-in-out group-hover:-translate-y-[250px]">
           <div className="text-[#344054] text-[36px] leading-none">“</div>
@@ -116,7 +121,7 @@ export default function Home() {
 
       {/* Work Experience Section */}
       <div className="w-full h-[827px] flex flex-col items-start mx-auto px-[71px]">
-        
+
         {/* Section Title */}
         <div className="w-full h-[234px] flex items-start justify-center space-x-2.5">
           <CustomeText title="My" className="font-medium text-6xl text-[#344054]" />
@@ -160,6 +165,171 @@ export default function Home() {
             ))}
           </div>
         </div>
+      </div>
+
+      {/* hire me */}
+      <div className="w-full h-[806px] gap-[96px] flex items-center justify-center px-[71px] py-[122px] bg-[#F2F4F7] rounded-[50px]">
+        <div className="relative w-[603px] h-[600px] group cursor-pointer">
+          {/* Default Image*/}
+          <Image
+            src="/Property 1=Default.svg"
+            alt="Hire me"
+            fill
+            className="object-contain z-10 transition-all duration-300 ease-in-out hover:translate-y-3"
+            priority
+          />
+
+          {/* Hover Image*/}
+          <Image
+            src="/Property 1=Variant2.svg"
+            alt="Hire me"
+            fill
+            className="object-contain absolute  -translate-y-[15px] transition-all duration-300 ease-in-out opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-120"
+            priority
+          />
+        </div>
+
+
+        <div className="w-[696px] h-[474px] flex flex-col items-start gap-[47px]">
+          <div className="w-fll h-12 flex gap-5">
+            <CustomeText
+              title="Why"
+              className="font-semibold text-6xl text-[#344054]"
+            />
+            <CustomeText
+              title="Hire me"
+              className="font-semibold text-6xl text-[#FD853A]"
+            />
+            <CustomeText
+              title="?"
+              className="font-semibold text-6xl text-[#344054]"
+            />
+          </div>
+          <p className="w-[444px] h-[78px] text-[#98A2B3] text-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus nunc, posuere in justo vulputate, bibendum sodales </p>
+          <div className="flex w-full h-[83px] items-start justify-start gap-2.5">
+            <div className="w-[343px] flex flex-col items-start justify-center">
+              <CustomeText
+                title="450+"
+                className="font-medium text-[36px] text-[#1D2939]"
+              />
+              <CustomeText
+                title="Project Completed"
+                className="text-[20px] text-[#667085]"
+              />
+            </div>
+            <div className="w-[343px] flex flex-col items-start justify-center">
+              <CustomeText
+                title="450+"
+                className="font-medium text-[36px] text-[#1D2939]"
+              />
+              <CustomeText
+                title="Project Completed"
+                className="text-[20px] text-[#667085]"
+              />
+            </div>
+          </div>
+
+          <button className="flex w-[233px] h-[108px] px-6 py-5 rounded-[24px] border border-[#151515] text-[#151515] font-semibold text-[32px] items-center justify-center transition-colors duration-300 ease-in-out cursor-pointer hover:bg-[#000000] hover:text-white">
+            Hire me
+          </button>
+
+        </div>
+      </div>
+
+      {/* my portfolio */}
+
+      <div className="w-full h-[1124px] flex flex-col items-center px-[71px] py-[97px] gap-12">
+
+        <div className="w-full h-[150px] flex justify-between items-center">
+          <div className="w-[643px] flex  flex-col items-start">
+            <CustomeText
+              title="Lets Have a look at"
+              className="font-semibold text-[64px] text-[#344054]"
+            />
+            <div className="w-full flex items-start justify-start gap-4">
+              <CustomeText
+                title="my"
+                className="font-semibold text-[64px] text-[#344054]"
+              />
+              <CustomeText
+                title="Portfolio"
+                className="font-semibold text-[64px] text-[#FD853A]"
+              />
+            </div>
+          </div>
+          <OrangeButton
+            title="See "
+          />
+        </div>
+
+        <div>
+          <div></div>
+          <div className="w-[947px] h-[54px] flex gap-[14px] items-center">
+            {buttons.map((text, index) => (
+              <button
+                key={index}
+                className="px-8 py-[15px] rounded-[24px] bg-[#F2F4F7] text-[#000000] text-[20px] hover:bg-[#FD853A] hover:text-white transition-colors duration-300 cursor-pointer"
+              >
+                {text}
+              </button>
+            ))}
+          </div>
+
+        </div>
+        <div className="flex flex-col w-[742px] h-[148px] items-start gap-6">
+          <div className="w-[742px] h-[63px] gap-[18px] flex items-start">
+            <CustomeText
+              title="Lirante - Food Delivery Solution"
+              className="font-bold text-[40px] text-[#344054]"
+            />
+            <div className="bg-[#FD853A] w-[48px] h-[48px] flex items-center justify-center p-3 rounded-[60px]">
+              <Image
+                src="/down right.svg"
+                alt="arrow"
+                width={34}
+                height={34}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <p className="w-full h-[61px] text-[20px] text-[#344054] text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue interdum ligula a dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis orci elementum egestas lobortis.</p>
+        </div>
+      </div>
+
+      {/* contact */}
+      <div className="w-full h-[476px] gap-2.5 flex flex-col py-[98px] items-center justify-center">
+        <div className="w-[799px] h-[150px] flex  flex-col items-start">
+          <CustomeText
+            title="Have an Awsome Project"
+            className="font-semibold text-[64px] text-[#344054]"
+          />
+          <div className="w-full flex items-start justify-start gap-4">
+            <CustomeText
+              title="Idea?"
+              className="font-semibold text-[64px] text-[#344054]"
+            />
+            <CustomeText
+              title="Let's Discuss"
+              className="font-semibold text-[64px] text-[#FD853A]"
+            />
+          </div>
+        </div>
+
+        <div className="w-[832px] h-[120px] flex flex-col items-center justify-center gap-2.5">
+          <div>
+            <Image
+              src="Frame 325.svg"
+              alt="message"
+              width={64}
+              height={58}
+            />
+          </div>
+          <input type="text" placeholder="Enter Email Address" 
+            className="font-medium text-[20px] w-full h-[86px] p-[14px] border border-[#E4E7EC] backdrop:blur-lg"
+          />
+          <button className="bg-[#FD853A] px-10 py-5 rounded-[60px]">Send</button>
+        </div>
+
       </div>
     </div>
   );
